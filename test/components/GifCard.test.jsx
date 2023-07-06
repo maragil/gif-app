7fc78bd44ddad0b0a8fdf1f2f7838271cb82e486
Maria <araujomaria84@gmail.com>
2023-07-06 18:24:47 +0200

    Add class 103

--- test/components/GifCard.test.jsx
+++ test/components/GifCard.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import {GifCard} from '../../src/components/GifCard';
 
 
@@ -9,9 +9,30 @@ describe('Pruebas en <GifCard />', () => {
 
     test('Debe de hacer match con el snapshot', () => {
 
-        const {container} = render ( <GifCard  title={title} url={url} />)
+        const {container} = render ( <GifCard  title={title} url={url} />);
         expect(container).toMatchSnapshot();
 
     });
+
+    test('Debe de mostrar la imagen con el URL y el ALT indicado', () => {
+
+        render ( <GifCard  title={title} url={url} />);
+        // screen.debug();
+
+        //tratar de tomar la imagen y preguntar si el src de la imagen es el mismo que el de la const url
+        const { src, alt } = screen.getByRole('img');
+        expect( src ).toBe( url );
+        expect( alt ).toBe( title );
+
+    });
+
+//asegurar que en el componente renderizado exista el título mostrado como un texto
+test('Debe de mostrar el título en el componente ', () => {
+
+    render ( <GifCard  title={title} url={url} />);
+    expect( screen.getByText( title )).toBeTruthy();
+
+});
+
 
 });
